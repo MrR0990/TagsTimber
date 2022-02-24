5e619a9ecfedc12cc9e0305104b120840edf4004
MrR <ruanyaguang@foxmail.com>
2022-02-24 17:35:24 +0800

    添加多tags逻辑
    添加多tags逻辑

--- timber-sample/src/main/java/com/example/timber/ExampleApp.java
+++ timber-sample/src/main/java/com/example/timber/ExampleApp.java
@@ -10,32 +10,36 @@ import androidx.annotation.NonNull;
 import timber.log.Timber;
 
 public class ExampleApp extends Application {
-  @Override public void onCreate() {
-    super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    if (BuildConfig.DEBUG) {
-      Timber.plant(new DebugTree());
-    } else {
-      Timber.plant(new CrashReportingTree());
-    }
-  }
-
-  /** A tree which logs important information for crash reporting. */
-  private static class CrashReportingTree extends Timber.Tree {
-    @Override protected void log(int priority, String tag, @NonNull String message, Throwable t) {
-      if (priority == Log.VERBOSE || priority == Log.DEBUG) {
-        return;
-      }
-
-      FakeCrashLibrary.log(priority, tag, message);
-
-      if (t != null) {
-        if (priority == Log.ERROR) {
-          FakeCrashLibrary.logError(t);
-        } else if (priority == Log.WARN) {
-          FakeCrashLibrary.logWarning(t);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new DebugTree().filter("LifeCycles"));
+        } else {
+            Timber.plant(new CrashReportingTree());
         }
-      }
     }
-  }
+
+    /**
+     * A tree which logs important information for crash reporting.
+     */
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, @NonNull String message, Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+            FakeCrashLibrary.log(priority, tag, message);
+
+            if (t != null) {
+                if (priority == Log.ERROR) {
+                    FakeCrashLibrary.logError(t);
+                } else if (priority == Log.WARN) {
+                    FakeCrashLibrary.logWarning(t);
+                }
+            }
+        }
+    }
 }
--- timber-sample/src/main/java/com/example/timber/ui/DemoActivity.java
+++ timber-sample/src/main/java/com/example/timber/ui/DemoActivity.java
@@ -13,22 +13,36 @@ import com.example.timber.databinding.DemoActivityBinding;
 import timber.log.Timber;
 
 public class DemoActivity extends Activity implements View.OnClickListener {
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    DemoActivityBinding binding = DemoActivityBinding.inflate(getLayoutInflater());
-    setContentView(binding.getRoot());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DemoActivityBinding binding = DemoActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    Timber.tag("LifeCycles");
-    Timber.d("Activity Created");
+        Timber.d("Activity Created");
+        Timber.tags("LifeCycles", "LifeCycles1", "LifeCycles2").d("Activity Created2");
 
-    binding.hello.setOnClickListener(this);
-    binding.hey.setOnClickListener(this);
-    binding.hi.setOnClickListener(this);
-  }
+        binding.hello.setOnClickListener(this);
+        binding.hey.setOnClickListener(this);
+        binding.hi.setOnClickListener(this);
+    }
 
-  @Override public void onClick(View v) {
-    Button button = (Button) v;
-    Timber.i("A button with ID %s was clicked to say '%s'.", button.getId(), button.getText());
-    Toast.makeText(this, "Check logcat for a greeting!", LENGTH_SHORT).show();
-  }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Timber.tags("LifeCycles", "LifeCycles1", "LifeCycles2").d("Activity onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Timber.tags("LifeCycles", "LifeCycles1", "LifeCycles2").d("Activity onStart");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        Timber.i("A button with ID %s was clicked to say '%s'.", button.getId(), button.getText());
+        Toast.makeText(this, "Check logcat for a greeting!", LENGTH_SHORT).show();
+    }
 }
